355ce54bfca3f34978839e6e31a16d4104e72901
Sinan Bir <f.sinanbir@gmail.com>
2017-12-14 01:24:26 +0300

    Solution files added.

---
+++ WebApp.IdentityServer.Example/Helper/MemoryDb.cs
@@ -0,0 +1,71 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+
+namespace WebApp.IdentityServer.Example.Helper
+{
+	public class MemoryDb
+	{
+		// scopes define the API resources in your system
+		public static IEnumerable<ApiResource> GetApiResources()
+		{
+			return new List<ApiResource>
+			{
+				new ApiResource("sinanbir.com.auth", "My API Display Name")
+			};
+		}
+
+		// clients
+		public static IEnumerable<Client> GetClients()
+		{
+			// client credentials client
+			return new List<Client>
+			{
+				new Client
+				{
+					ClientId = "client1",
+					AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+					ClientSecrets =
+					{
+						new Secret("password".Sha256())
+					},
+					AllowedScopes = { "sinanbir.com.auth" }
+				},
+ 
+                // resource owner password grant client
+                new Client
+				{
+					ClientId = "ropcclient",
+					AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+					AllowOfflineAccess = true,//Enables refresh token.
+                    ClientSecrets =
+					{
+						new Secret("password".Sha256())
+					},
+					AllowedScopes = { "sinanbir.com.auth", IdentityServerConstants.StandardScopes.OfflineAccess, }
+				}
+			};
+		}
+
+		public static List<TestUser> GetUsers()
+		{
+			return new List<TestUser>
+			{
+				new TestUser
+				{
+					SubjectId = "1",
+					Username = "sinan",
+					Password = "bir"
+				},
+				new TestUser
+				{
+					SubjectId = "2",
+					Username = "testuser",
+					Password = "password"
+				}
+			};
+		}
+	}
+}
---
+++ WebApp.IdentityServer.Example/Startup.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using WebApp.IdentityServer.Example.Helper;
+
+namespace WebApp.IdentityServer.Example
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+
+			var authUrl = "http://localhost:8000";
+
+			#region IdentityServerSetup
+
+			services.AddIdentityServer()
+				.AddDeveloperSigningCredential()
+				.AddInMemoryApiResources(MemoryDb.GetApiResources())
+				.AddInMemoryClients(MemoryDb.GetClients())
+				.AddTestUsers(MemoryDb.GetUsers());
+
+			#endregion
+
+			#region Validation Settings
+			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+			.AddJwtBearer(options =>
+			{
+				options.Authority = authUrl;
+				options.RequireHttpsMetadata = false;
+				options.TokenValidationParameters = new TokenValidationParameters
+				{
+					ValidateIssuer = true,
+					ValidIssuer = authUrl,
+					ValidateAudience = false,
+					ValidAudience = "sinanbir.com.auth",//test purposes
+					ValidateLifetime = true,
+				};
+			});
+
+			#endregion
+			services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+			
+			app.UseIdentityServer();
+			app.UseAuthentication();
+			app.UseMvc();
+        }
+    }
+}
